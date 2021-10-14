b44d7fbc52b55fa467105922c6bc035cfc42d2bb
sergey b <sbarchshevskiy@gmail.com>
2021-10-14 13:03:07 -0400

    simplify car data fetching

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import Models from "./components/Models";
-import SearchBar from "./components/SearchBar";
-import axios from 'axios';
-import './App.css'
+import FetchAPI from "./components/FetchAPI";
+
 
 function App() {
-
-  const [models, setModels] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [query, setQuery] = useState('volvo')
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const fetchedData = await
-        axios (`https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformakeyear/make/${query}/vehicleType/car?format=json`)
-      console.log(fetchedData.data["Results"])
-      setModels(fetchedData.data["Results"])
-      setLoading(false)
-    }
-    fetchData();
-  }, [query])
-
-
-  return (
-    <div className="container">
-      <SearchBar getQuery={(q) => setQuery(q)}/>
-      <Models models={models} loading={loading}/>
-    </div>
-  );
+    return(
+        <div className="App">
+            <FetchAPI/>
+        </div>
+    )
 }
 
 export default App;
---
+++ client/src/components/FetchAPI.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+import {useEffect, useState} from "react";
+import {logDOM} from "@testing-library/react";
+
+
+export default function FetchAPI(){
+
+    const [models, setModels] = useState(null)
+    const [query, setQuery] = useState(null)
+
+    const handleChange = (event) => {
+        setQuery(event.target.value)
+    }
+
+
+    useEffect(() => {
+        async function fetchData(){
+            await axios.get(`https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformakeyear/make/${query}/vehicleType/car?format=json`)
+                .then((res) => {
+                    const result = res.data["Results"]
+                    result.map((res) => {
+                        const model = res.Model_Name
+                        console.log(model)
+                        setModels(model)
+                    })
+                }).catch(err => console.log(err))
+        }
+        fetchData()
+    },[query])
+
+
+    return(
+        <div>
+            <form>
+                <input onChange={handleChange}/>
+            </form>
+            <div className="results">
+                {models.slice(0,20)}
+            </div>
+        </div>
+    )
+}
